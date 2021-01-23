668aede4fc6e6e962d13c2dd5aa54ac1bfb84fb8
smurf211 <mspadaro@student.sjcny.edu>
2021-01-22 20:25:03 -0500

    fully synced, capsule moves with players correctly.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,6 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
+
 namespace DaggerfallWorkshop.Game
 {
     public class PlayerMove : NetworkBehaviour
@@ -10,6 +15,8 @@ namespace DaggerfallWorkshop.Game
 
         [SyncVar(hook = nameof(OnHolaCountChanged))]
         int holaCount = 0;
+
+       
         private GameObject playerAdvancedObj = null;
        
         private GameObject playerUnitObj = null;
@@ -17,24 +24,29 @@ namespace DaggerfallWorkshop.Game
         void HandleMovement()
         {
             if (isLocalPlayer)
-            {   
+            {
+                    
                
 
 
-            
-                //Vector3 playerAdvancedPosition = playerAdvancedObj.transform.position;
-               // playerAdvancedPosition.x = playerAdvancedPosition.x -2f;
-                //playerUnitObj.transform.position = playerAdvancedPosition;
+                Vector3 playerAdvancedPosition = playerAdvancedObj.transform.position;
+                Debug.Log("Player Advanced Pos: " + playerAdvancedPosition);
+                playerAdvancedPosition.x = playerAdvancedPosition.x -2f;
+                transform.position = playerAdvancedPosition;
 
 
+
+                // Debug.Log("IS LOCAL PLAYER!!");
+
+                //float moveHorizontal = Input.GetAxis("Horizontal");
+                //float moveVertical = Input.GetAxis("Vertical");
+                //Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+               //transform.position = transform.position + movement;
+           
                 
-               // Debug.Log("IS LOCAL PLAYER!!");
                 
-                float moveHorizontal = Input.GetAxis("Horizontal");
-                float moveVertical = Input.GetAxis("Vertical");
-                Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
-                transform.position = transform.position + movement;
-               
+                
+
             }
             else{
                
@@ -45,16 +57,16 @@ namespace DaggerfallWorkshop.Game
 
         void Update()
         {
-            playerAdvancedObj = GameObject.Find("PlayerAdvanced");
+            //playerAdvancedObj = GameObject.Find("PlayerAdvanced");
             
-            playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
+            //playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
 
             HandleMovement();
             if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
             {
                 Debug.Log("sending hola to server!");
                 Hola();
-                HandleMovement();
+               
             }
              
         //Debug.Log("Player Position: X = " + playerAdvancedObj.transform.position.x + " --- Y = " + playerAdvancedObj.transform.position.y + " --- Z = " + 
@@ -73,25 +85,33 @@ namespace DaggerfallWorkshop.Game
         public override void OnStartServer()
         {
             Debug.Log("Player has been spawned on the server!");
-            playerAdvancedObj = GameObject.Find("PlayerAdvanced");
-            playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
+            /*
+            if (isLocalPlayer)
+            {
+                playerAdvancedObj = GameObject.Find("PlayerAdvanced");
+                playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
+            }
+
+*/
 
 
 
 
-            
-        
-            
         }
 
         public override void OnStartClient(){
-            
-            playerAdvancedObj = GameObject.Find("PlayerAdvanced");
-            playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
 
-            Vector3 playerAdvancedPosition = playerAdvancedObj.transform.position;
-            playerAdvancedPosition.x = playerAdvancedPosition.x -2f;
-            playerUnitObj.transform.position = playerAdvancedPosition;
+            if (isLocalPlayer)
+            {
+                playerAdvancedObj = GameObject.Find("PlayerAdvanced");
+                playerUnitObj = GameObject.Find("PlayerUnit(Clone)");
+
+                Vector3 playerAdvancedPosition = playerAdvancedObj.transform.position;
+                playerAdvancedPosition.x = playerAdvancedPosition.x -2f;
+                playerUnitObj.transform.position = playerAdvancedPosition;
+            }
+
+            
               
             //for(int i =0; i < playerUnitObj.Length; i++){
            // Debug.Log("THE ARRAY OF THE PLAYERUNIT " + playerUnitObj[i].ToString());
@@ -112,6 +132,12 @@ namespace DaggerfallWorkshop.Game
 
         [TargetRpc]
         void ReplyHola()
+        {
+            Debug.Log("Received hola from server");
+        }
+
+        [TargetRpc]
+        void getPlayerAdvPos()
         {
             Debug.Log("Received hola from server");
         }
